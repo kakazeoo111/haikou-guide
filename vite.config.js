8eb054416297470b0156c795e27974116443fa2a
苏家永 <test@example.com>
2026-02-01 19:56:21 +0800

    Modify vite.config.js to set build directory

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // 设置构建输出目录为 'build'
+  },
 })
